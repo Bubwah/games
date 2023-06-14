537e81dc1c59f9a37a292d576e4b609e367eaa39
Bubwah <laurens-christian@hotmail.com>
2023-06-14 19:06:40 +0200

    uihfds

--- racecar game/racegame.py
+++ racecar game/racegame.py
@@ -4,6 +4,7 @@ import random
 import time
 
 from pathlib import Path
+
 pygame.init()
 
 
@@ -35,6 +36,21 @@ class Racegame:
         self.truckImg = pygame.image.load('truck.png')
         self.truckImg = pygame.transform.scale(self.truckImg, (100, 200))
 
+        self.racecarBlueImg = pygame.image.load('racecar_blue.png')
+        self.racecarBlueImg = pygame.transform.rotate(pygame.transform.scale(self.racecarBlueImg, (100, 140)), 180)
+
+        self.racecarGreenImg = pygame.image.load('racecar_green.png')
+        self.racecarGreenImg = pygame.transform.rotate(pygame.transform.scale(self.racecarGreenImg, (100, 140)), 180)
+
+        self.racecarPinkImg = pygame.image.load('racecar_pink.png')
+        self.racecarPinkImg = pygame.transform.rotate(pygame.transform.scale(self.racecarPinkImg, (100, 140)), 180)
+
+        self.racecarYellowImg = pygame.image.load('racecar_yellow.png')
+        self.racecarYellowImg = pygame.transform.rotate(pygame.transform.scale(self.racecarYellowImg, (100, 140)), 180)
+
+        self.motorcycleImg = pygame.image.load('motorcycle.png')
+        self.motorcycleImg = pygame.transform.rotate(pygame.transform.scale(self.motorcycleImg, (90, 130)), 180)
+
         self.start_Img = pygame.image.load("start_button.png")
         self.exit_Img = pygame.image.load("exit_button.png")
 
@@ -45,18 +61,27 @@ class Racegame:
         self.introImg = pygame.transform.scale(self.introImg, (700, 700))
 
         self.delete_obstacles = []
+        self.obstacle_dict = {"car": [0.2, self.obstacleImg,
+                                      (self.obstacleImg.get_width(), self.obstacleImg.get_height())],
+                              "green_car": [0.2, self.racecarGreenImg,
+                                            (self.racecarGreenImg.get_width(), self.racecarGreenImg.get_height())],
+                              "yellow_car": [0.2, self.racecarYellowImg,
+                                             (self.racecarYellowImg.get_width(), self.racecarYellowImg.get_height())],
+                              "pink_car": [0.2, self.racecarPinkImg,
+                                           (self.racecarPinkImg.get_width(), self.racecarPinkImg.get_height())],
+                              "blue_car": [0.2, self.racecarBlueImg,
+                                           (self.racecarBlueImg.get_width(), self.racecarBlueImg.get_height())],
+                              "motorcycle": [0.2, self.motorcycleImg, (60, self.motorcycleImg.get_height())],
+                              "truck": [0.05, self.truckImg,
+                                        (self.truckImg.get_width(), self.truckImg.get_height())]}
+
+        self.game_exit = False
 
     def draw_car(self, x, y):
         self.gameDisplay.blit(self.carImg, (x, y))
 
-    def draw_obstacle(self, obstacle_x, obstacle_y, obstacle_type):
-        if obstacle_type == "truck":
-            img = self.truckImg
-        else:
-            # default obstacle is car
-            img = self.obstacleImg
-
-        self.gameDisplay.blit(img, (obstacle_x, obstacle_y))
+    def draw_obstacle(self, obstacle_x, obstacle_y, obstacle_type, obstacle_img):
+        self.gameDisplay.blit(obstacle_img, (obstacle_x, obstacle_y))
 
     def draw_text(self, text, font):
         text_surface = font.render(text, True, self.green)
@@ -133,20 +158,18 @@ class Racegame:
         x = (self.display_width * 0.45)
         y = (self.display_height * 0.75)
         obstacle_x_list = [30, 160, 300, 440, 570]
-        obstacle_dims = {"truck": (self.truckImg.get_width(), self.truckImg.get_height()),
-                         "car": (self.obstacleImg.get_width(), self.obstacleImg.get_height())}
 
         x_change = 0
-
-        obstacle_speed = 4
         dodged = 0
-        spawnrate = min(0.01 + 0.5*dodged, 1)
-        game_exit = False
+
+        self.game_exit = False
 
         obstacles = []
 
-        while not game_exit:
-            max_cars = min(dodged // 6 + 1, 3)
+        while not self.game_exit:
+            obstacle_speed = min(6 + 0.4 * dodged, 16)
+            spawnrate = min(0.02 + 0.008 * dodged, 0.33)
+            max_cars = min(dodged // 10 + 1, 3)
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
@@ -170,21 +193,29 @@ class Racegame:
             if abs(scroll) > self.bg_height:
                 scroll = 0
 
-
             if random.random() < spawnrate and len(obstacles) < max_cars:
-                if random.random() > 0.85:
-                    obstacle_type = "truck"
-                else:
-                    obstacle_type = "car"
-                obstacles.append([random.choice(obstacle_x_list), -600, obstacle_type])
+                random_obstacle = random.choices([(k, v) for k, v in self.obstacle_dict.items()],
+                                                 weights=[obst[0] for obst in self.obstacle_dict.values()],
+                                                 k=1)[0]
+                random_obstacle_type = random_obstacle[0]
+                # values: [spawnrate, img, (img_width, img_height)]
+                random_obstacle_values = random_obstacle[1]
+                random_obstacle_img = random_obstacle_values[1]
+                obstacle_dims = random_obstacle_values[2]
+
+                spawn_x = random.choice(obstacle_x_list)
+                while spawn_x in [obstacle[0] for obstacle in obstacles if obstacle[1] < obstacle_dims[1]]:
+                    spawn_x = random.choice(obstacle_x_list)
+
+                obstacles.append([spawn_x, -600, random_obstacle_type, random_obstacle_img])
 
             self.draw_car(x, y)
 
             for obstacle_index in range(len(obstacles)):
                 obstacle = obstacles[obstacle_index]
-                self.draw_obstacle(obstacle[0], obstacle[1], obstacle[2])
+                self.draw_obstacle(obstacle[0], obstacle[1], obstacle[2], obstacle[3])
                 obstacle[1] += obstacle_speed
-                obstacle_width, obstacle_height = obstacle_dims[obstacle[2]]
+                obstacle_width, obstacle_height = self.obstacle_dict[obstacle[2]][2]
 
                 if y < obstacle[1] + obstacle_height:
                     print('y crossover')
@@ -195,20 +226,12 @@ class Racegame:
 
                         self.crash()
                         self.display_highscore(dodged)
-                        game_exit = True
+                        self.game_exit = True
 
                 if obstacle[1] > self.display_height:
                     self.delete_obstacles.append(obstacle_index)
 
                     dodged += 1
-                    obstacle_speed_change = 0.5
-
-
-                    if obstacle_speed == 16:
-                        obstacle_speed_change = 0
-
-
-                    obstacle_speed += obstacle_speed_change
 
             self.draw_highscore(dodged)
 
@@ -219,9 +242,9 @@ class Racegame:
             self.delete_obstacles = []
 
             if x > self.display_width - self.car_width or x < 0:
-                    self.crash()
-                    self.display_highscore(dodged)
-                    game_exit = True
+                self.crash()
+                self.display_highscore(dodged)
+                self.game_exit = True
 
             pygame.display.update()
             self.clock.tick(60)
--- snake game/Snakegame.py
+++ snake game/Snakegame.py
@@ -1,6 +1,8 @@
 import pygame
 import time
 import button
+import random
+
 pygame.init()
 
 class Snakegame:
@@ -24,39 +26,77 @@ class Snakegame:
         self.blue = (0, 0, 255)
         self.red = (255, 0 , 0)
         self.white = (255, 255, 255)
+        self.black = (0, 0, 0)
 
-        self.x1 = 300
-        self.y1 = 300
+        self.x1 = self.dis_width / 2
+        self.y1 = self.dis_height / 2
+
+        self.snake_block = 10
+
+        self.snake_speed = 20
+
+        self.small_text = pygame.font.Font('freesansbold.ttf', 50)
+        self.large_text = pygame.font.Font('freesansbold.ttf', 85)
+
+        self.snake_list = []
+        self.snake_length = 1
 
         self.x1_change = 0
         self.y1_change = 0
 
+        self.Game_Exit = False
+
         self.start_Img = pygame.image.load('start_button.png')
         self.exit_Img = pygame.image.load("exit_button.png")
 
+        self.foodx = round(random.randrange(0, self.dis_width - self.snake_block) / 10.0) * 10.0
+        self.foody = round(random.randrange(0, self.dis_width - self.snake_block) / 10.0) * 10.0
+
         self.clock = pygame.time.Clock()
+
+        self.intro = True
 
     def draw_text(self, text, font):
         text_surface = font.render(text, True, self.green)
         return text_surface, text_surface.get_rect()
 
+    def spawn_snake(self):
+        for x in self.snake_list:
+            pygame.draw.rect(self.dis, self.blue, [x[0], x[1], self.snake_block, self.snake_block])
+
+    def spawn_food(self):
+        pygame.draw.rect(self.dis, self.black, [self.foodx, self.foody, self.snake_block, self.snake_block])
+
+    def display_highscore(self):
+        with open('highscore.txt', "r") as f:
+            highscore_lines = f.readlines()[0]
+        with open("highscore.txt", "w") as v:
+            if self.snake_length >= int(highscore_lines):
+                v.write(str(self.snake_length - 1))
+            else:
+                v.write(highscore_lines)
+
+    def draw_highscore(self, count):
+        font = pygame.font.SysFont(None, 25)
+        text = font.render("Maximum Food eaten: " + str(count), True, self.green)
+        self.dis.blit(text, (0, 0))
 
     def game_intro(self):
         self.dis.blit(self.intro_bg, (0,0))
-
-
         self.intro = True
-        small_text = pygame.font.Font('freesansbold.ttf', 50)
-        large_text = pygame.font.Font('freesansbold.ttf', 85)
-        text_surf, text_rect = self.draw_text("Snake", large_text)
+
+        text_surf, text_rect = self.draw_text("Snake", self.large_text)
         text_rect.center = ((self.dis_width / 2), (self.dis_height / 3.5))
         self.dis.blit(text_surf, text_rect)
+        self.foodx = round(random.randrange(0, self.dis_width - self.snake_block) / 10.0) * 10.0
+        self.foody = round(random.randrange(0, self.dis_width - self.snake_block) / 10.0) * 10.0
 
         while self.intro:
             start_button = button.Button(400, 400, self.start_Img, 0.20, self.dis)
             exit_button = button.Button(200, 400, self.exit_Img, 0.04, self.dis)
-            for self.event in pygame.event.get():
-                if self.event.type == pygame.QUIT:
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
                     pygame.quit()
                     quit()
 
@@ -66,39 +106,76 @@ class Snakegame:
 
             if start_button.clicked:
                 self.game_loop()
+
+            self.draw_highscore(self.snake_length)
             pygame.display.update()
 
     def game_loop(self):
-        Game_Exit = False
 
-        while not Game_Exit:
+        self.Game_Exit = False
+
+        self.x1 = self.dis_width / 2
+        self.y1 = self.dis_height / 2
+
+        self.snake_list = []
+
+        self.x1_change = 0
+        self.y1_change = 0
+
+        while not self.Game_Exit:
+            self.dis.fill(self.white)
+
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                    Game_Exit = True
+                    self.Game_Exit = True
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_LEFT:
-                        self.x1_change = -10
+                        self.x1_change = - self.snake_block
                         self.y1_change = 0
                     elif event.key == pygame.K_RIGHT:
-                        self.x1_change = 10
+                        self.x1_change = + self.snake_block
                         self.y1_change = 0
                     elif event.key == pygame.K_UP:
-                        self.y1_change == -10
-                        self.x1_change == 0
+                        self.y1_change = - self.snake_block
+                        self.x1_change = 0
                     elif event.key == pygame.K_DOWN:
-                        self.y1_change == 10
-                        self.x1_change == 0
+                        self.y1_change = + self.snake_block
+                        self.x1_change = 0
+
+            if self.x1 >= self.dis_width or self.x1 < 0 or self.y1 >= self.dis_height or self.y1 < 0:
+                self.Game_Exit = True
 
             self.x1 += self.x1_change
             self.y1 += self.y1_change
 
             self.dis.fill(self.white)
-            pygame.draw.rect(self.dis, self.blue, [200, 150, 10, 10])
-            pygame.display.update()
+            pygame.draw.rect(self.dis, self.black, [self.foodx, self.foody, self.snake_block, self.snake_block])
+
+            snake_head = [self.x1, self.y1]
+            self.snake_list.append(snake_head)
+            if len(self.snake_list) > self.snake_length:
+                del self.snake_list[0]
+
+            for x in self.snake_list[:-1]:
+               if x == snake_head:
+                 self.Game_Exit = True
+            self.spawn_snake()
+
+            if self.x1 == self.foodx and self.y1 == self.foody:
+                self.foodx = round(random.randrange(0, self.dis_width - self.snake_block) / 10.0) * 10.0
+                self.foody = round(random.randrange(0, self.dis_height - self.snake_block) / 10.0) * 10.0
+                self.snake_length += 1
+
             self.clock.tick(30)
+        pygame.display.update()
+        text_surf, text_rect = self.draw_text("you lost", self.large_text)
+        text_rect.center = ((self.dis_width / 2), (self.dis_height / 2))
+        self.display_highscore()
+        self.dis.blit(text_surf, text_rect)
+        pygame.display.update()
 
+        time.sleep(4)
+        self.game_intro()
 
-        pygame.quit()
-        quit()
 
 
